c1aaeab4f72e21591916663084df3bf27a2ada88
Sergey Pomortsev <zaooooza@gmail.com>
2026-03-09 12:13:26 +0500

    fixed vercel config 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'app',
-  ssr: false,
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt','@nuxtjs/google-fonts'],
